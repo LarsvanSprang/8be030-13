df5e9e9ec67693da6d6ed1d6326990c123f90571
pjoep <j.l.prinsen@student.tue.nl>
2025-05-21 11:54:50 +0200

    downscaler stack function with stepwise alignment

--- project1.py
+++ project1.py
@@ -8,13 +8,15 @@ import registration as reg
 import matplotlib.pyplot as plt
 import numpy as np
 from skimage import io, transform
+import uuid
+
 
 def intensity_based_registration_rigid(s1, s2):
 
     # read the fixed and moving images
     # change these in order to read different images
-    I = plt.imread(f'../data/image_data/{s1}.tif')
-    Im = plt.imread(f'../data/image_data/{s2}.tif')
+    I = plt.imread(f'image_data/{s1}.tif')
+    Im = plt.imread(f'image_data/{s2}.tif')
 
     # initial values for the parameters
     # we start with the identity transformation
@@ -92,8 +94,8 @@ def intensity_based_registration_affine(s1, s2):
 
     # read the fixed and moving images
     # change these in order to read different images
-    I = plt.imread(f'../data/image_data/{s1}.tif')
-    Im = plt.imread(f'../data/image_data/{s2}.tif')
+    I = plt.imread(f'image_data/{s1}.tif')
+    Im = plt.imread(f'image_data/{s2}.tif')
 
     # initial values for the parameters
     # we start with the identity transformation
@@ -171,8 +173,8 @@ def intensity_based_registration_affine_mi(s1, s2):
 
     # read the fixed and moving images
     # change these in order to read different images
-    I = plt.imread(f'../data/image_data/{s1}.tif')
-    Im = plt.imread(f'../data/image_data/{s2}.tif')
+    I = plt.imread(f'image_data/{s1}.tif')
+    Im = plt.imread(f'image_data/{s2}.tif')
 
     # initial values for the parameters
     # we start with the identity transformation
@@ -247,6 +249,7 @@ def intensity_based_registration_affine_mi(s1, s2):
 
         
 def downsampler(steps, end, tIm):
+
     stack = []
     for i in range(steps):
         res = end-i*(end/steps)
@@ -255,3 +258,87 @@ def downsampler(steps, end, tIm):
     stack.append(tIm)
     
     return stack
+
+def intensity_based_registration_affine_th(I, Im):
+    # initial values for the parameters
+    # we start with the identity transformation
+    # most likely you will not have to change these
+    x = np.array([0., 1. ,1. ,0.,0.,0.,0.])
+
+    # NOTE: for affine registration you have to initialize
+    # more parameters and the scaling parameters should be
+    # initialized to 1 instead of 0
+
+    # the similarity function
+    # this line of code in essence creates a version of rigid_corr()
+    # in which the first two input parameters (fixed and moving image)
+    # are fixed and the only remaining parameter is the vector x with the
+    # parameters of the transformation
+    fun = lambda x: reg.affine_corr(I, Im, x, return_transform=False)
+
+    # the learning rate
+    mu = 0.001
+
+    # number of iterations
+    num_iter = 200
+
+    iterations = np.arange(1, num_iter+1)
+    similarity = np.full((num_iter, 1), np.nan)
+
+    fig = plt.figure(figsize=(14,6))
+
+    # fixed and moving image, and parameters
+    ax1 = fig.add_subplot(121)
+
+    # fixed image
+    im1 = ax1.imshow(I)
+    # moving image
+    im2 = ax1.imshow(I, alpha=0.7)
+    # parameters
+    txt = ax1.text(0.3, 0.95,
+        np.array2string(x, precision=5, floatmode='fixed'),
+        bbox={'facecolor': 'white', 'alpha': 1, 'pad': 10},
+        transform=ax1.transAxes)
+
+    # 'learning' curve
+    ax2 = fig.add_subplot(122, xlim=(0, num_iter), ylim=(0, 1))
+
+    learning_curve, = ax2.plot(iterations, similarity, lw=2)
+    ax2.set_xlabel('Iteration')
+    ax2.set_ylabel('Similarity')
+    ax2.grid()
+    log = ""
+
+    # perform 'num_iter' gradient ascent updates
+    for k in np.arange(num_iter):
+
+        # gradient ascent
+        g = reg.ngradient(fun, x)
+        x += g*mu
+
+        # for visualization of the result
+        S, Im_t, Th = reg.affine_corr(I, Im, x, return_transform=True)
+
+        clear_output(wait = True)
+
+        # update moving image and parameters
+        im2.set_data(Im_t)
+        txt.set_text(np.array2string(x, precision=5, floatmode='fixed'))
+
+        # update 'learning' curve
+        similarity[k] = S
+        learning_curve.set_ydata(similarity)
+        if (not np.isnan(S)):
+            log = S
+            Th_last = Th
+        else:
+            break
+
+        display(fig)
+    
+    key = uuid.uuid4().hex[:4]  # 8-character random key
+    fig.savefig(f'output/result_{key}.png')
+    plt.close(fig)
+    print(log)
+    print(Th_last)
+    return Th_last
